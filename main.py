4fd9d652012faea33c747b705d1197cf88bc2605
rezadarbandsari <reza.darbandsari@alphacertlabs.com>
2020-01-13 21:53:19 +1300

    Add Gorilla Sprites

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from apartment import Apartment
 from utils import *
 import pygame
 import time
+import os
 
 # Initializing pygame
 pygame.init()
@@ -12,8 +13,14 @@ pygame.init()
 building_colors = ((0x00, 0x55, 0xdd), (0xcf, 0xcf, 0xcf), (0xcf, 0x00, 0x00))
 window_colors = ((0xf9, 0xa6, 0x02), (0x69, 0x69, 0x69))
 
+APARTMENT_MIN_WIDTH = 90
+APARTMENT_MAX_WIDTH = 120
+APARTMENT_MIN_HEIGHT = 200
+APARTMENT_MAX_HEIGHT = 350
+
+
 # Setting up the screen
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 768
 SCREEN_RESOLUTION = (SCREEN_WIDTH, SCREEN_HEIGHT)
 BACKGROUND_COLOR = (0x00, 0x8d, 0xff)
@@ -24,9 +31,9 @@ screen = pygame.display.set_mode(SCREEN_RESOLUTION, pygame.DOUBLEBUF, 32)
 
 apartments = []
 
-for i in range(10):
-	h = randint(100, 300)
-	w = randint(90, 120)
+for i in range(SCREEN_WIDTH // APARTMENT_MIN_WIDTH):
+	w = randint(APARTMENT_MIN_WIDTH, APARTMENT_MAX_WIDTH)
+	h = randint(APARTMENT_MIN_HEIGHT, APARTMENT_MAX_HEIGHT)
 	t = SCREEN_HEIGHT - h
 	if i > 0:
 		l += apartments[i-1].rect.width
@@ -37,6 +44,8 @@ for i in range(10):
 	ap = Apartment(a, c)
 	apartments.append(ap)
 
+gorilla_idle = pygame.image.load("sprites\\gorilla-idle.jpg").convert()
+gorilla_throwing = pygame.image.load("sprites\\gorilla-throwing.jpg").convert()
 
 # The Mainloop
 done = False
@@ -45,9 +54,16 @@ while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				done = True
 	
 	for ap in apartments:
 		generate_building(screen, ap.rect, ap.color)
+
+	screen.blit(gorilla_idle, (apartments[1].rect.left + apartments[1].rect.width // 2 - 50/2, apartments[1].rect.top - 50))
+	screen.blit(gorilla_idle, (apartments[-1].rect.left + apartments[-2].rect.width // 2 - 50/2, apartments[-2].rect.top - 50))
 	pygame.display.update()
 	time.sleep(1/60)
 
